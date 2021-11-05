eb80b3b8f1902a863d9507b6f066da0630acfbed
Sascha Presnac <sascha@presnac.de>
2021-11-05 19:33:11 +0100

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])
-    ->post('/log', [ApiController::class, 'log'])
-    ->name('api.log');
+Route::prefix('/v2')
+    ->middleware(['auth:sanctum'])
+    ->group(function() {
+        Route::post('/log', [ApiController::class, 'log'])->name('api.log');
+});
