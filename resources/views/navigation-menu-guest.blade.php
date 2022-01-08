69e4d5079d4c05980064f700d20bd56f2b15317d
Sascha Presnac <sascha@presnac.de>
2022-01-08 01:29:01 +0100

    wip

--- resources/views/navigation-menu-guest.blade.php
+++ resources/views/navigation-menu-guest.blade.php
@@ -12,6 +12,19 @@
                         {{ __('Home') }}
                     </x-jet-nav-link>
                 </div>
+                @if (Route::has('login') && config('logcrawler_server.enable_visible_login'))
+                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-200 underline">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-200 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-200 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
             </div>
         </div>
     </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,7 +20,24 @@
                     </p>
                     <div class="flex justify-center mb-12 md:mb-0">
                         <a class="bg-sky-700 hover:bg-sky-600 text-white font-bold py-4 px-6 rounded shadow-lg"
-                           href="https://github.com/spresnac/logcrawler-server">Clone and start 🚀</a>
+                           href="https://github.com/spresnac/logcrawler-server">Clone and start <i class="fab fa-git-alt"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="py-6 sm:px-6 bg-white border-b border-gray-200 text-center">
+                    <h2 class="font-semibold text-2xl text-gray-800 leading-tight">Want to give something back?</h2>
+                    <p class="mb-6">
+                        I would be very thankfull for any help. Logcrawler is free and open source, so you can clone your copy and modify it; you can even send your improvements back as pull request. This way, you help logcrawler grow.<br>
+                        Don't forget to give logcrawler a star on github to grow it's popularity 😉
+                    </p>
+                    <p class="mb-6">
+                        If coding is not your thing, you can donate something to me. There are several ways to do this, the most common could be to participate in sponsoring me on github.
+                    </p>
+                    <div class="flex justify-center mb-12 md:mb-0">
+                        <a class="bg-sky-700 hover:bg-sky-600 text-white font-bold py-4 px-6 rounded shadow-lg"
+                           href="https://github.com/sponsors/spresnac"><i class="fab fa-github"></i> Sponsor me on github</a>
                     </div>
                 </div>
             </div>
