592bbda265506dc6860642cd96bdbcc7d7f300c6
Sascha Presnac <sascha@presnac.de>
2023-04-08 20:12:39 +0200

    Updating vendors
    - let the projectkey be selectable ;)

--- resources/views/livewire/projectlist.blade.php
+++ resources/views/livewire/projectlist.blade.php
@@ -4,10 +4,10 @@
     </div>
     <table class="table-fixed w-full mb-16 mt-6 px-4 py-3 shadow-md border-slate-200 text-gray-600 responsive rounded-lg">
         <thead class="text-left bg-slate-200 text-slate-400">
-            <th class="px-4 py-2 w-20 font-normal text-right">ID</th>
+            <th wire:click="order_by('id')" class="px-4 py-2 w-20 font-normal text-right">ID</th>
             <th class="px-4 py-2 w-60 font-normal">{{ __('project.owner') }}</th>
-            <th class="px-4 py-2 font-normal">{{ __('project.name') }}</th>
-            <th class="px-4 py-2 w-60 font-normal text-center ">{{ __('project.last_log_date') }}</th>
+            <th wire:click="order_by('name')" class="px-4 py-2 font-normal">{{ __('project.name') }}</th>
+            <th wire:click="order_by('logs.logged_at')" class="px-4 py-2 w-60 font-normal text-center ">{{ __('project.last_log_date') }}</th>
             <th class="px-4 py-2 text-right w-36 font-normal">{{ __('project.entries.count') }}</th>
             <th class="px-4 py-2 text-center w-36 font-normal">{{ __('project.info') }}</th>
             <th class="px-4 py-2 text-right w-20 font-normal">{{ __('project.actions') }}</th>
