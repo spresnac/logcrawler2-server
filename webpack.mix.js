a023bb48d56f9a8c31ab8b2d3adf039a4044f814
Shift <shift@laravelshift.com>
2025-02-15 18:58:05 +0000

    Remove `webpack.mix.js`

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require('laravel-mix');
-mix.disableNotifications();
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
-mix.postCss('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/css/fa-all.css');
-mix.version();
