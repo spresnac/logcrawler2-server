8eace818fb22b0f2ed57b8286e8fbe6b7636a9fd
Shift <shift@laravelshift.com>
2025-02-15 18:58:05 +0000

    Use new `@vite` Blade directive

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" href="{{ mix('css/fa-all.css') }}">
+        @vite('resources/css/app.css')
+        @vite('resources/css/fa-all.css')
         @livewireStyles
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
